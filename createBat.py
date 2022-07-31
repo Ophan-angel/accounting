afe5897d6e79d92885b9085e81af5dbc30224eff
Ophan-angel <108130531+Ophan-angel@users.noreply.github.com>
2022-07-31 12:07:29 +0200

    script to create batch for startup

---
+++ createBat.py
@@ -0,0 +1,27 @@
+import os
+
+class Create():
+    def __init__(self):
+        self.__file_name = "accouting.bat"
+        self._createBat() 
+
+    def _createBat(self):
+        try:
+            current_link = os.getcwd()
+            print("<< [*] The used link is :: {}".format(current_link))
+
+            current_work = current_link + "\\accounting.py"
+            with open(self.__file_name, "w") as file: 
+                file.write("@Echo off\n")
+                file.write("title Accoutning\n")
+                file.write("color 0a\n")
+                file.write("python {}\n".format(current_work))
+                file.write("pause")
+        
+        except Exception as e:
+            print("<< [!] Exception while trying to create the requested batch file :: {}".format(e))
+
+def main():
+    obj = Create() 
+
+main() 
